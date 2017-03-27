58b8bedfa6d933e2188f75e4a93fc90c0c55b50f
Emil Haugaard <emil.haugaard@hotmail.com>
2017-03-27 14:59:54 +0200

    EmployeeTest

--- tests/EmployeeTest.java
+++ tests/EmployeeTest.java
@@ -7,14 +7,27 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class EmployeeTest {
     /*
-        Test add employee with a giving ID, and then make th
-     */
+        Add an employees to the "firm", and check if they can add a project
+    */
     @Test
     public void addEmployee(){
         SystemTimeManager STM = new SystemTimeManager();
         assertEquals(STM.employees.size(),0);
+
         Employee employee = new Employee("SteveJobs");
+        STM.employees.add(employee);
+        assertEquals(STM.employees.size(),1);
 
+    }
 
+    @Test
+    public void addProject(){
+        assertEquals(STM.projects.size(),0);
+        employee.AddProject("Design New Sodamachine", 1);
+        assertEquals(STM.projects.size(),1);
+
+        Project project = STM.projects.get(0);
+        assertEquals(project.getProjectName(),"Design New SodaMachine");
+        assertEquals(project.getStartWeek, 1);
     }
 }
