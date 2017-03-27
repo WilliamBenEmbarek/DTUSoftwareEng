9993077ad37f91dc4d0f00c692206a578145e9cd
Emil Haugaard <emil.haugaard@hotmail.com>
2017-03-27 14:36:24 +0200

    Adding tets classes

--- tests/EmployeeTest.java
+++ tests/EmployeeTest.java
@@ -1,5 +1,20 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by Emil on 27/03/2017.
  */
 public class EmployeeTest {
+    /*
+        Test add employee with a giving ID, and then make th
+     */
+    @Test
+    public void addEmployee(){
+        SystemTimeManager STM = new SystemTimeManager();
+        assertEquals(STM.employees.size(),0);
+        Employee employee = new Employee("SteveJobs");
+
+
+    }
 }
