f59a1edff5ba533742a5d507b98beb972b1a0397
William BenEmbarek <william.benembarek@gmail.com>
2017-03-27 14:33:57 +0200

    Added Constructors to various files

--- src/Employee.java
+++ src/Employee.java
@@ -4,9 +4,18 @@
 public class Employee {
 	String ID;
 	String CurrentProject;
+	int[][] TimeManagementArray;
 
-	public Employee(String ID, String CurrentProject, ) {
-		this.ID
+	public Employee(String ID, int[][] timeManagementArray) {
+		this.ID = ID;
+		TimeManagementArray = timeManagementArray;
 	}
 
+	public Employee(String ID, String currentProject, int[][] timeManagementArray) {
+		this.ID = ID;
+		CurrentProject = currentProject;
+		TimeManagementArray = timeManagementArray;
+	}
+
+	public void AddProject
 }
