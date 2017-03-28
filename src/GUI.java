c18657a7f75a2cf0d72a7e70a7d4b66e9e4bc188
Emil Haugaard <emil.haugaard@hotmail.com>
2017-03-28 22:50:07 +0200

    Login

--- src/GUI.java
+++ src/GUI.java
@@ -3,25 +3,105 @@
  */
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.*;
 
 public class GUI extends JFrame implements ActionListener{
+    private String user;
+    private String pass;
+    private JTextField userName;
+    private JTextField password;
+    private JButton login;
+
+    // Panels
+    private JPanel loginPanel;
 
     public static void main(String[] args) {
-        GUI simplegui = new GUI();
 
-        simplegui.setTitle("SimpleGui"); // Set title on window
-        simplegui.setSize(600, 400);     // Set size
-        simplegui.setResizable(false);    // Allow the window to be resized
-        simplegui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        simplegui.setVisible(true);      // Make the window visible
-    }
+        GUI mainFrame = new GUI();
 
-    public void actionPerformed(ActionEvent e) {
+        windowProperties(mainFrame);
     }
 
     public GUI(){
+        loginPage();
+    }
 
+    public void loginPage(){
+        loginPanel = new JPanel(new GridBagLayout());
+
+        // Sets contrains to organize components in the panel.
+        GridBagConstraints cs = new GridBagConstraints();
+        cs.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel labelUsername = new JLabel("Username: ");
+        cs.gridx     = 0;
+        cs.gridy     = 0;
+        cs.gridwidth = 1;
+        loginPanel.add(labelUsername, cs);
+
+        userName     = new JTextField(20);
+        cs.gridx     = 1;
+        cs.gridy     = 0;
+        cs.gridwidth = 2;
+        loginPanel.add(userName, cs);
+
+        JLabel labelPassword = new JLabel("Password: ");
+        cs.gridx     = 0;
+        cs.gridy     = 1;
+        cs.gridwidth = 1;
+        loginPanel.add(labelPassword, cs);
+
+        password     = new JPasswordField(20);
+        cs.gridx     = 1;
+        cs.gridy     = 1;
+        cs.gridwidth = 2;
+        loginPanel.add(password, cs);
+
+
+        login        = new JButton("Login");
+        cs.gridx     = 1;
+        cs.gridy     = 2;
+        cs.gridwidth = 2;
+        login.addActionListener(this);
+        loginPanel.add(login, cs);
+
+        getContentPane().add(loginPanel);
+    }
+
+    // Hardcoding of login
+    public boolean checkLogin(){
+        user = userName.getText().trim();
+        pass = password.getText().trim();
+
+        if(user.equals("Emil") && pass.equals("123")){
+            return true;
+        }
+        if(user.equals("William") && pass.equals("321")){
+            return true;
+        }
+        return false;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == login) {
+            if(checkLogin()) {
+                JLabel myLabel = new JLabel("test");
+                getContentPane().removeAll();
+                revalidate();
+                repaint();
+                getContentPane().add(myLabel);
+                revalidate();
+                repaint();
+            }
+        }
+    }
+
+    public static void windowProperties(JFrame mainFrame){
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(400, 200);
+        mainFrame.setLayout(new GridBagLayout());
+        mainFrame.setVisible(true);
     }
 }
