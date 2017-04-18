4532c3f5470e310a51ab11789b0f172189402066
William BenEmbarek <william.benembarek@gmail.com>
2017-04-18 12:21:37 -0400

    Updating sample data

--- tests/SampleDataSetup.java
+++ tests/SampleDataSetup.java
@@ -5,18 +5,17 @@ import org.junit.Before;
  */
 public class SampleDataSetup {
 	SystemTimeManager STM = new SystemTimeManager();
+	Employee e1 = new Employee("Emil");
+	Employee e2 = new Employee("William");
+	Employee e3 = new Employee("Test person");
+	Project testProject = new Project("TestProject",1);
+	ProjectLeader projectManager = new ProjectLeader("201701","testProject",testProject);
 	@Before
 	public void setUp() throws Exception{
-		Employee e1 = new Employee("Emil");
+
 		STM.Employees.add(e1);
-		Employee e2 = new Employee("William");
 		STM.Employees.add(e2);
-		Employee e3 = new Employee("Test person");
 		STM.Employees.add(e3);
-
-		Project testProject = new Project("TestProject",1);
-
-		ProjectLeader projectManager = new ProjectLeader("201701","testProject",testProject);
 		STM.ProjectLeaders.add(projectManager);
 
 	}
