afc1788835876462fbd839c257355a10c0dc62e7
William Ben Embarek <william.benembarek@gmail.com>
2017-05-07 21:27:58 +0200

    Comments in unit tests

--- tests/TestAssignActivities.java
+++ tests/TestAssignActivities.java
@@ -22,19 +22,19 @@ public class TestAssignActivities extends SampleDataSetup {
 		Employee test = stm.getEmployeeByID(ID);
 		ProjectLeader p1 = stm.getProjectLeaders().get(0);
 		Project testP = stm.getProjects().get(0);
-		ArrayList<ProjectActivity> activities = testP.getActivities(stm.getCurrentWeek());
+		ArrayList<ProjectActivity> activities = testP.getActivities(stm.getCurrentWeek()); //Set up employee, project leader, project and activities list
 		//Step 1
-		assertEquals(ID, test.getID());
+		assertEquals(ID, test.getID()); //Check if the employee test's ID is acutally "Test person".
 
 		//Step 2
-		int numActivites = test.getAssignedActivites().size();
+		int numActivites = test.getAssignedActivites().size(); //Check total number of assigned activities should be 0
 		assertEquals(numActivites, 0); // Number Activities = 0
 		// step 3
 		for (int i = 0;i<activities.size();i++) {
 			p1.assignEmployee(test,activities.get(i));
 		}
 		test.refreshActivties();
-		assertEquals(activities,test.getAssignedActivites());
+		assertEquals(activities,test.getAssignedActivites()); //Check if the employees assigned activities matches the activities list.
 
 	}
 }
--- tests/TimeRegistrationTest.java
+++ tests/TimeRegistrationTest.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
  * Created by Emil on 27/03/2017
  */
 public class TimeRegistrationTest extends SampleDataSetup{
-/*
+
 	@Test
 	public void testRegisterHours() throws Exception {
 		//Step 0
@@ -25,15 +25,15 @@ public class TimeRegistrationTest extends SampleDataSetup{
 		String ID = "Test person";
 		Employee test = stm.getEmployeeByID(ID);
 		ProjectLeader p1 = stm.getProjectLeaders().get(0);
-		Project testP = stm.getProjects().get(0);
-		ArrayList<ProjectActivity> activities = testP.getActivities();
+		Project testP = stm.getProjects().get(0); //Set up employees
+		ArrayList<ProjectActivity> activities = testP.getActivities(stm.getCurrentWeek()); // Get a list of the activities
 
-		for (ProjectActivity activity : activities) {
+		for (ProjectActivity activity : activities) { // For every activity in the activities have Project Leader P1 assign it to employee Test
 			p1.assignEmployee(test, activity);
 		}
 		//Step 1
-		test.refreshActivties();
-		test.registerHours(1, 1,10.0);
+		test.refreshActivties(); // refresh the activities in the employees personal 3d array
+		test.registerHours(1, 1,10.0); //Register 10 hours on monday
 		assertEquals(test.getHoursWorkedDay(stm.getCurrentWeek(), 1), 10,0);
 
 	}
@@ -45,7 +45,7 @@ public class TimeRegistrationTest extends SampleDataSetup{
 		Employee test = stm.getEmployeeByID(ID);
 		ProjectLeader p1 = stm.getProjectLeaders().get(0);
 		Project testP = stm.getProjects().get(0);
-		ArrayList<ProjectActivity> activities = testP.getActivities();
+		ArrayList<ProjectActivity> activities = testP.getActivities(stm.getCurrentWeek()); //Same as before
 		for (ProjectActivity activity : activities) {
 			p1.assignEmployee(test, activity);
 		}
@@ -54,12 +54,12 @@ public class TimeRegistrationTest extends SampleDataSetup{
 		test.registerHours(1.0, 1,4.0);
 		test.registerHours(6.0,1,3.0);
 		test.registerHours(0.0,1,2.0);
-		assertEquals(test.getHoursWorkedDay(stm.getCurrentWeek(), 1), 9,0);
+		assertEquals(test.getHoursWorkedDay(stm.getCurrentWeek(), 1), 9,0); //Assign 4 hours to activity 1, 3 to actvity 6 and 2 to activity 0, total is 9
 
-		test.registerHours(6,1,8);
-		test.registerHours(1,1,2);
-		assertEquals(test.getHoursWorkedDay(stm.getCurrentWeek(),1),12,0);
+		test.registerHours(6.0,1,8);
+		test.registerHours(1.0,1,2); //Edit the total number of hours for activities 6 and 1
+		assertEquals(test.getHoursWorkedDay(stm.getCurrentWeek(),1),12,0); //New total is now 12 hours
 
 	}
-	*/
+
 }
